dab730adba672dbd409c373cc634b03822b160b0
Matthew <matthewseaber@outlook.com>
2026-05-23 18:33:05 +0100

    Added current time fetching and display on the admin dashboard

---
+++ app/api/timer/get_time/route.ts
@@ -0,0 +1,63 @@
+import { Query } from "appwrite";
+import { tablesDB } from "@/lib/appwrite";
+
+import { NextResponse } from "next/server";
+
+const databaseID = process.env.APPWRITE_DATABASE_ID!;
+const timerSessionsTableID = process.env.APPWRITE_TIMER_SESSIONS_TABLE_ID!;
+
+export async function GET() {
+  try {
+    let timerStatus = "stopped";
+    let startTime = null;
+    let baseTime = 0;
+
+    const activeSessions = await tablesDB.listRows(
+      databaseID,
+      timerSessionsTableID,
+      [Query.equal("active", true)],
+    );
+
+    const activeSession = activeSessions.rows[0];
+
+    if (activeSession) {
+      timerStatus = "running";
+      startTime = activeSession.start_time;
+    } else {
+      timerStatus = "stopped";
+      startTime = null;
+    }
+
+    const previousSessions = await tablesDB.listRows(
+      databaseID,
+      timerSessionsTableID,
+      [
+        Query.equal("active", false),
+        Query.greaterThanEqual(
+          "start_time",
+          new Date(new Date().setHours(0, 0, 0, 0)).toISOString(),
+        ),
+      ],
+    );
+
+    if (previousSessions.total > 0) {
+      for (const session of previousSessions.rows) {
+        const sessionStartTime = new Date(session.start_time).getTime();
+        const sessionEndTime = new Date(session.end_time).getTime();
+        const duration = Math.floor((sessionEndTime - sessionStartTime) / 1000);
+
+        baseTime += duration;
+      }
+    } else {
+      baseTime = 0;
+    }
+
+    return NextResponse.json({ timerStatus, startTime, baseTime });
+  } catch (error) {
+    console.log("Error: " + error);
+    return NextResponse.json(
+      { error: "Error fetching session data" },
+      { status: 500 },
+    );
+  }
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect, useState } from "react";
 
 function DashboardPage() {
   const [status, setStatus] = useState("stopped");
   const [time, setTime] = useState(0);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [baseTime, setBaseTime] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   function formatTime(timeInSeconds: number) {
     const minutes = Math.floor(timeInSeconds / 60);
@@ -13,6 +17,48 @@ function DashboardPage() {
 
     return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   }
+
+  async function getTimerData() {
+    try {
+      const res = await fetch("api/timer/get_time", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setStatus(data.timerStatus);
+        setBaseTime(data.baseTime);
+        setTime(data.baseTime);
+        setStartTime(
+          data.startTime ? new Date(data.startTime).getTime() : null,
+        );
+
+        setLoading(false);
+      } else {
+        alert("Error fetching timer data.");
+      }
+    } catch (error) {
+      alert("Error fetching timer data.");
+      console.log("Error message: " + error);
+    }
+  }
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (status === "running" && startTime) {
+      interval = setInterval(() => {
+        const currentElapsedTime = Math.floor((Date.now() - startTime) / 1000);
+
+        setTime(baseTime + currentElapsedTime);
+      }, 1000);
+    } else {
+      return;
+    }
+
+    return () => clearInterval(interval);
+  }, [status, startTime, baseTime]);
 
   async function startTimer() {
     try {
@@ -24,7 +70,8 @@ function DashboardPage() {
 
       if (res.ok) {
         setStatus("running");
-        const startTime = await res.json();
+        const data = await res.json();
+        setStartTime(new Date(data.startTime).getTime());
       } else {
         alert("Error starting the timer.");
         window.location.reload();
@@ -44,6 +91,11 @@ function DashboardPage() {
 
       if (res.ok) {
         setStatus("stopped");
+        const newBaseTime =
+          baseTime + Math.floor((Date.now() - (startTime || 0)) / 1000);
+        setStartTime(null);
+        setBaseTime(newBaseTime);
+        setTime(newBaseTime);
       } else {
         alert("Error ending the timer.");
         window.location.reload();
@@ -54,13 +106,25 @@ function DashboardPage() {
     }
   }
 
+  useEffect(() => {
+    const pageLoad = async () => {
+      await getTimerData();
+    };
+
+    pageLoad();
+  }, []);
+
   return (
     <div>
       <h1 className="text-3xl font-semibold">Admin Dashboard</h1>
       <div className="justify-center text-center">
-        <p className="my-8 text-5xl tracking-wider font-semibold">
-          {formatTime(time)}
-        </p>
+        {loading ? (
+          <Skeleton className="w-48 h-12 my-8 mx-auto" />
+        ) : (
+          <p className="my-8 text-6xl h-12 tracking-wider font-semibold">
+            {formatTime(time)}
+          </p>
+        )}
         {status === "stopped" && (
           <Button onClick={() => startTimer()}>Start session</Button>
         )}
